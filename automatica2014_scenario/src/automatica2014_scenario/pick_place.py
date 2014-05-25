ad601e9a93263cc356182f89207260daf617e833
Your Name <you@example.com>
2014-05-25 23:38:21 +0200

    int to float

--- automatica2014_scenario/src/automatica2014_scenario/pick_place.py
+++ automatica2014_scenario/src/automatica2014_scenario/pick_place.py
@@ -42,7 +42,7 @@ def make_top_grasp(pose):
     grasp.id = "top"
 
     # open
-    grasp.pre_grasp_posture = make_joint_trajectory(['finger_left_joint','finger_right_joint'],[1,1])
+    grasp.pre_grasp_posture = make_joint_trajectory(['finger_left_joint','finger_right_joint'],[1.0,1.0])
 
     # close
     grasp.grasp_posture = make_joint_trajectory(['finger_left_joint','finger_right_joint'],[0.01,0.01])
