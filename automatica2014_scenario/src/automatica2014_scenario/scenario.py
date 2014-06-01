fd3c8de3253d5cd486fc5b48a789f4a0ea069f3b
ros <ros@scenario.(none)>
2014-06-01 10:58:34 +0200

    extended scenario script

--- automatica2014_scenario/src/automatica2014_scenario/scenario.py
+++ automatica2014_scenario/src/automatica2014_scenario/scenario.py
@@ -56,38 +56,48 @@ class MoveHomeState(smach.State):
         self.robot_interfaces = robot_interfaces
         self.home_targets = home_targets
     def execute(self, userdata):
-        if False: #userdata.robot_interface.move(*self.home_targets[userdata.active_robot], others = userdata.blocked_poses + userdata.pickable_poses ) == 1:
+        ####### TEST #######
+        #rospy.sleep(5)
+        #return 'done'
+        res = self.robot_interfaces[userdata.active_robot].move(*self.home_targets[userdata.active_robot], others = userdata.blocked_poses + userdata.pickable_poses )
+        print res
+        if  res == 1:
            return 'done'
         else:
            return 'error'
         
 class SelectObjectsState(smach.State):
-    CANDIATES_BACK = [ (0,0), (0,0), (0,0), (0,0),]
-    CANDIATES_FRONT = [ (1,0), (1,0), (1,0), (1,0),]
-    def __init__(self):
+    CANDIATES_BACK = [ (0.4,0.9),  (0.4,1.1),]
+    CANDIATES_FRONT = [ (0.85,1.045),]
+    def __init__(self, robot_interfaces):
         smach.State.__init__(self,
-            outcomes = ['done','not_found','error'],
-            input_keys = [],
+            outcomes = ['place','pick','not_found','error'],
+            input_keys = ['active_robot'],
             output_keys = ['blocked_poses', 'pickable_poses','place_candidates'])
         self.lock = threading.Lock()
         self.poses = []
         self.sub_poses = rospy.Subscriber("/automatica_poses", PoseArray, self.poses_cb)
         self.pick = 'front'
+        self.robot_interfaces = robot_interfaces
        
     def poses_cb(self, msg):
         with self.lock:
             self.poses = msg.poses
             
     def execute(self, userdata):
+        rospy.sleep(0.5) # for viewer
         back = []
         front = []
+        grasped = self.robot_interfaces[userdata.active_robot].is_grasped()
         with self.lock:
             for p in self.poses:
-                if p.x > 0.5:
+                if p.position.x > 0.5:
                     front.append(p)
                 else:
                     back.append(p)
-        if len(front) == 0 and len(back) == 0:
+        if grasped:
+           pass # use last
+        elif len(front) == 0 and len(back) == 0:
             userdata.blocked_poses = []
             userdata.pickable_poses = []
             userdata.place_candidates = []
@@ -101,11 +111,11 @@ class SelectObjectsState(smach.State):
             userdata.blocked_poses = back
             userdata.pickable_poses = front
             userdata.place_candidates = self.CANDIATES_BACK
-        elif self.pick == 'front':
+        elif self.pick == 'back':
             userdata.blocked_poses = front
             userdata.pickable_poses = back
             userdata.place_candidates = self.CANDIATES_FRONT
-        return 'done'
+        return 'pick' if not grasped else 'place'
 
 class PickState(smach.State):
     def __init__(self, robot_interfaces):
@@ -114,6 +124,9 @@ class PickState(smach.State):
                 input_keys = ['active_robot', 'blocked_poses','pickable_poses'])
         self.robot_interfaces = robot_interfaces
     def execute(self, userdata):
+        ####### TEST #######
+        #rospy.sleep(5)
+        #return 'done'
         res = self.robot_interfaces[userdata.active_robot].pick_one_of(userdata.pickable_poses, userdata.blocked_poses)
         if res == 1:
            return 'done'
@@ -126,10 +139,14 @@ class PlaceState(smach.State):
     def __init__(self, robot_interfaces):
         smach.State.__init__(self,
                 outcomes = ['done','not_reached', 'error'],
-                input_keys = ['active_robot', 'blocked_poses','pickable_poses'])
+                input_keys = ['active_robot', 'place_candidates','blocked_poses','pickable_poses'])
         self.robot_interfaces = robot_interfaces
     def execute(self, userdata):
+        ####### TEST #######
+        #rospy.sleep(5)
+        #return 'done'
         res = self.robot_interfaces[userdata.active_robot].place_somewhere(userdata.place_candidates, userdata.pickable_poses + userdata.blocked_poses)
+        print res 
         if res == 1:
            return 'done'
         elif res == 2: # TODO
@@ -146,7 +163,7 @@ if __name__ == "__main__":
     }
     home_targets = {
             'sia10f': (['sia10f_'+l for l in ['joint_s','joint_l','joint_e','joint_u','joint_r','joint_b','joint_t']],[0,0,0,0,0,0,0]),
-            'ur5': (['ur5_'+l for l in ['shoulder_pan_joint','shoulder_lift_joint','elbow_joint','wrist_1_joint','wrist_2_joint','wrist_3_joint']],[0,0,0,0,0,0,0]),
+            'ur5': (['ur5_'+l for l in ['shoulder_pan_joint','shoulder_lift_joint','elbow_joint','wrist_1_joint','wrist_2_joint','wrist_3_joint']],[1.751, -2.4174, 1.438, -2.1035, -1.5672, 1.8285]),
     }
     
 
@@ -158,15 +175,15 @@ if __name__ == "__main__":
     with sm:
         # Add states to the container
         smach.StateMachine.add('CHOOSE_ROBOT', ChooseRobotState(robot_interfaces), 
-    transitions={'done':'SELECT_OBJECTS','error':'exit'})
-        smach.StateMachine.add('SELECT_OBJECTS', SelectObjectsState(), 
-    transitions={'done':'PICK_OBJECT','not_found':'MOVE_HOME','error':'exit'})
+    transitions={'done':'DETECT_OBJECTS','error':'exit'})
+        smach.StateMachine.add('DETECT_OBJECTS', SelectObjectsState(robot_interfaces), 
+    transitions={'pick':'PICK_OBJECT','place':'PLACE_OBJECT','not_found':'MOVE_HOME','error':'CHOOSE_ROBOT'})
         smach.StateMachine.add('PICK_OBJECT', PickState(robot_interfaces), 
-    transitions={'done':'PLACE_OBJECT','not_reached':'MOVE_HOME','error':'exit'})
+    transitions={'done':'DETECT_OBJECTS','not_reached':'MOVE_HOME','error':'CHOOSE_ROBOT'})
         smach.StateMachine.add('PLACE_OBJECT', PlaceState(robot_interfaces), 
-    transitions={'done':'SELECT_OBJECTS','not_reached':'MOVE_HOME','error':'exit'})
+    transitions={'done':'DETECT_OBJECTS','not_reached':'MOVE_HOME','error':'CHOOSE_ROBOT'})
         smach.StateMachine.add('MOVE_HOME', MoveHomeState(robot_interfaces, home_targets), 
-        transitions={'done':'CHOOSE_ROBOT','error':'exit'})
+        transitions={'done':'CHOOSE_ROBOT','error':'CHOOSE_ROBOT'})
 
     # Execute SMACH plan
     sis = smach_ros.IntrospectionServer('Automatica2014', sm, '/')
